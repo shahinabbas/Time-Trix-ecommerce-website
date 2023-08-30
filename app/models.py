4f9a5696d9fa674ee0eec9baa8c285005f96b355
shahinabbas <shahinabbas771@gmail.com>
2023-08-30 15:26:53 +0530

    fix2

--- app/models.py
+++ app/models.py
@@ -44,7 +44,7 @@ class CustomUser(AbstractUser):
     otp = models.CharField(_('OTP'), max_length=6, blank=True)
     is_otp_verified = models.BooleanField(default=False)
 
-    USERNAME_FIELD = 'username'
+    USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
     objects = CustomUserManager()
